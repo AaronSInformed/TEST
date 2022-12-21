7134ccd7000f9e08b05aa83da21ced303044d5c4
Aaron <Aaron@Aaron.com>
2022-12-21 12:49:52 +0000

    Senstive info removed

--- MoreSensitiveInfo.java
+++ MoreSensitiveInfo.java
@@ -2,7 +2,7 @@ public class MoreSensitiveInfo {
 
 	public static void main(String[] args) {
 		System.out.println("========================");
-		System.out.println("========***REMOVED***========");
+		System.out.println("================");
 		System.out.println("========================");
     }
 }
